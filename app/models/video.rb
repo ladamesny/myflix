ec77a4646da0fd9332840c76a2d624edbeda7c0f
Larry Adames <adames.larry@gmail.com>
2015-08-10 23:52:38 -0400

    created search by title feature

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,8 @@ class Video < ActiveRecord::Base
   belongs_to :category
 
   validates_presence_of :title, :description
+
+  def self.search_by_title term
+    self.where("title LIKE ?", "%#{term}%") || []
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -5,3 +5,22 @@ describe Video do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
 end
+
+describe '#search_by_title' do
+  it "should return an empty array" do
+    expect(Video.search_by_title("Batman")).to eq([])
+  end
+
+  it "should return one matches" do
+    batman = Video.create(title: "Batman", description: "First Batman")
+    Video.create(title: "The Dark Knight", description: "Best Batman")
+    expect(Video.search_by_title("Batman")).to eq([batman])
+  end
+
+  it "should return two matches" do
+    batman = Video.create(title: "Batman", description: "First Batman")
+    batman_returns = Video.create(title: "Batman Returns", description: "Second Batman")
+    Video.create(title: "The Dark Knight", description: "Best Batman")
+    expect(Video.search_by_title("Batman")).to eq([batman, batman_returns])
+  end
+end
