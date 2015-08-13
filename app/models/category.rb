1d68dced9f26e5c0f001ed82b6de3b71ccd258be
Larry Adames <adames.larry@gmail.com>
2015-08-13 19:24:36 -0400

    Implemented #recent_videos via TDD

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,10 @@
 class Category < ActiveRecord::Base
-  has_many :videos, -> { order("title")}
+  has_many :videos, -> { order("created_at DESC")}
 
   validates_presence_of :name
+
+  def recent_videos
+    videos.limit(6)
+    # Video.where("category = ?", self.name ).order(created_at: :desc).limit(6)
+  end
 end
